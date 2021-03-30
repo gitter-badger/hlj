19000a69978e8319184724c905e79357b6f97199
Seaborn Lee <seabornlee.cn@gmail.com>
2021-03-31 07:55:07 +0800

    test: move reporter test together

--- test/hlj.test.js
+++ test/hlj.test.js
@@ -21,29 +21,4 @@ describe('JavaScript TDD framework', () => {
     const stdout = exec(`hlj ${FIXTURE}/failed.test.js`);
     expect(stdout).toContain('FAIL');
   });
-  describe('Test report', () => {
-    it('should output number of tests passed', () => {
-      const stdout = exec(`hlj ${FIXTURE}/two-tests.test.js`);
-      expect(stdout).toContain(
-        `Tests: ${getSuccessfulReport('2 passed')}, 2 total`
-      );
-    });
-    it('should output number of tests passed while there are failed tests', () => {
-      const stdout = exec(`hlj ${FIXTURE}/passed-and-failed.test.js`);
-      expect(stdout).toContain(
-        `Tests: ${getFailedReport('1 failed')}, ${getSuccessfulReport(
-          '1 passed'
-        )}, 2 total\n`
-      );
-    });
-
-    it('should output execution time', () => {
-      const stdout = exec(`hlj ${FIXTURE}/passed-and-failed.test.js`);
-      const expectSuffix = /([0-9]*[.])?[0-9]+s/;
-      const expectPrefix = /Time:/;
-
-      expect(stdout).toMatch(expectPrefix);
-      expect(stdout).toMatch(expectSuffix);
-    });
-  });
 });
--- test/report/consoleReporter.test.js
+++ test/report/consoleReporter.test.js
@@ -28,4 +28,28 @@ describe('Console Reporter', () => {
     const stdout = exec(`hlj ${FIXTURE}/really-complex.test.js`);
     expect(stdout).toMatch(/\d+ ms/);
   });
+
+  it('should output number of tests passed', () => {
+    const stdout = exec(`hlj ${FIXTURE}/two-tests.test.js`);
+    expect(stdout).toContain(
+      `Tests: ${getSuccessfulReport('2 passed')}, 2 total`
+    );
+  });
+  it('should output number of tests passed while there are failed tests', () => {
+    const stdout = exec(`hlj ${FIXTURE}/passed-and-failed.test.js`);
+    expect(stdout).toContain(
+      `Tests: ${getFailedReport('1 failed')}, ${getSuccessfulReport(
+        '1 passed'
+      )}, 2 total\n`
+    );
+  });
+
+  it('should output execution time', () => {
+    const stdout = exec(`hlj ${FIXTURE}/passed-and-failed.test.js`);
+    const expectSuffix = /([0-9]*[.])?[0-9]+s/;
+    const expectPrefix = /Time:/;
+
+    expect(stdout).toMatch(expectPrefix);
+    expect(stdout).toMatch(expectSuffix);
+  });
 });
