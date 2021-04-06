f71e0b96ff14441b5e9f0edef831a79cf11c4850
guzhongren <guzhongren@live.cn>
2021-04-06 21:10:23 +0800

    pref: rename

---
+++ fixture/describe-only/fdescribe-nested.test.js
@@ -0,0 +1,17 @@
+describe('Not run this describe ', () => {
+  it('Not run case: should 1 equal to 1', function () {
+    expect(1).toBe(2);
+  });
+});
+
+fdescribe('run it', () => {
+  it('should 1 equal to 1', function () {
+    expect(1).toBe(1);
+  });
+
+  fdescribe('run again', () => {
+    it('should 1 equal to 1', () => {
+      expect(1).toBe(1);
+    });
+  });
+});
--- test/describe-only.test.js
+++ test/describe-only.test.js
@@ -2,7 +2,7 @@ const { exec } = require('./helper/exec');
 const { green, yellow } = require('../src/report/render');
 const { FIXTURE } = require('./helper/fixtures');
 
-describe('Keyword', () => {
+describe('only run fdescribe function in a suite', () => {
   it('should run it as test', function () {
     const stdout = exec(`hlj ${FIXTURE}/describe-only/fdescribe.test.js`);
 
@@ -10,5 +10,18 @@ describe('Keyword', () => {
     expect(stdout).toContain(`Test Suites: ${green('1 passed')}, 1 total`);
     expect(stdout).toContain(`${yellow('1 skipped')}`);
     expect(stdout).toContain(`${green('2 passed')}, 3 total`);
+  });
+});
+
+describe('only run fdescribe nested in a suite', () => {
+  it('should return 1 skipped 2 passed and 3 total', () => {
+    const stdout = exec(
+      `hlj ${FIXTURE}/describe-only/fdescribe-nested.test.js`
+    );
+
+    expect(stdout).toContain('PASS');
+    expect(stdout).toContain('1 skipped');
+    expect(stdout).toContain('2 passed');
+    expect(stdout).toContain('3 total');
   });
 });
